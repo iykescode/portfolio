e9355b1ee02b652fd77130dc1c4d09ed95288ec0
iykescode <chuksikechukwu15@gmail.com>
2024-01-08 16:06:49 +0000

    Initial commit

---
+++ javascript/typed.js
@@ -0,0 +1,9 @@
+new Typed(
+    ".typed",
+    {
+        strings:["Java Developer", "PHP Developer", "UI/UX Designer"],
+        typeSpeed:100,
+        backSpeed:100,
+        backDelay:1000,
+        loop:true
+    });
